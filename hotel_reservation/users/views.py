c8fcb6dcbd4a496eee4d9e49063aa46ba549885e
mmajje <bluekmky@likelion.org>
2020-06-19 18:51:55 +0900

    build static

--- hotel_reservation/users/views.py
+++ hotel_reservation/users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def mypage(request):
+    return render (request, "myPage/mypage.html")
+
+def signup(request):
+    return render (request, "login/signup.html")
+    
+def signin(request):
+    return render (request, "login/signin.html")
