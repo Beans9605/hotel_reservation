51487b3c35386bf3579cbac1ef5f5976d4a8f634
Bean9605 <alkad1234@likelion.org>
2020-06-19 15:41:30 +0900

    111

--- hotel_reservation/users/models.py
+++ hotel_reservation/users/models.py
@@ -5,6 +5,7 @@ class Users(models.Model):
     username=models.CharField(max_length=6)
     number=models.IntegerField()
     birth=models.DateField()
+    password=models.CharField(max_length=20)
 
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
