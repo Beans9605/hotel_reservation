6d6f1e56a457c23478a798cd28f95abe72c9a1ce
jeongyoungbyoun <63783625+jeongyoungbyoun@users.noreply.github.com>
2020-06-22 11:41:29 +0900

    login_1

--- hotel_reservation/users/views.py
+++ hotel_reservation/users/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
-#회원가입
-#로그인
-#로그아웃
+
 def login(request):
     if request.method == 'post':   #post의 방식으로 정보를 가져오겠습니다
         username = request.POST['username']  # username 가져오기
@@ -16,4 +13,5 @@ def login(request):
             return render(request, 'login.html',{'error': 'username or password is not correct'})
     else:
         return render(request, 'login.html')
+
 
