46cd2494b3e4690fce8cc8dffaffbfcf2fcdc2c0
Bean9605 <alkad1234@likelion.org>
2020-06-23 13:54:56 +0900

    commit_to_integrity

--- hotel_reservation/users/urls.py
+++ hotel_reservation/users/urls.py
@@ -1,5 +1,10 @@
 
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path("<pk:int>/", views.mypage, name="mypage"),
+    path('login/', views.login, name="login"),
+    path('logout/', views.logout, name="logout"),
+    path('signup/', views.sign_up, name='signup'),
 ]
--- hotel_reservation/users/views.py
+++ hotel_reservation/users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Users
 
 def logout(request):
@@ -25,24 +25,42 @@ def sign_up(request):
         user.save()
 
 def mypage(request):
-    if request.session.get['user',True] :
+    if request.session.get['user', True] :
         user = Users.objects.filter(username = request.session['user'])
         reservations = Reservation.objects.filter(user=user)
         context={'user':user, 'reservations':reservations}
 
 
 
-def login(request):
-    if request.method == 'post':   #post의 방식으로 정보를 가져오겠습니다
-        username = request.POST['username']  # username 가져오기
-        password = request.PODT['password']  # password 가져오기
-        user = auth.authenticate(username = username, password = password)
-        if user is not None:
-            auth.login(request, user)
+# def login(request):
+#     if request.method == 'post':   #post의 방식으로 정보를 가져오겠습니다
+#         username = request.POST['username']  # username 가져오기
+#         password = request.PODT['password']  # password 가져오기
+#         user = auth.authenticate(username = username, password = password)
+#         if user is not None:
+            # 해당 구문은 auth 모듈을 사용한 걸로, 장고에서 제공하는 유저모델을 사용할때 사용 가능하다.
+#             auth.login(request, user)
+#             return redirect('home')
+#         else:
+#             return render(request, 'login.html',{'error': 'username or password is not correct'})
+#     else:
+#         return render(request, 'login.html')
+
+
+def login(request) :
+    if request.method == 'POST' :
+        username = request.POST['username']
+        password = request.POST['password']
+        # get_object_or_404는 해당 데이터를 검색하고 만약 없을 시에 404 not found 페이지를 보여줌
+        user = get_object_or_404(Users, username=username)
+
+        if password == user.password :
+            request.session['user'] = user.username
             return redirect('home')
-        else:
-            return render(request, 'login.html',{'error': 'username or password is not correct'})
-    else:
-        return render(request, 'login.html')
 
-
+        else :
+            context = {'err' : "비밀번호가 틀렸습니다."}
+            return redirect('login', context)
+        
+    else :
+        return render(request, "", {"err" : "형식이 잘못됐습니다."})
